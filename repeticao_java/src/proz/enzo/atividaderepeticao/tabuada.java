ad2c34b0e3bd8b9d418f1dc3a886c44696096a0d
Mateus Lopes <mateustlopes11@gmail.com>
2024-06-04 18:29:36 -0300

    mensagem

--- repeticao_java/src/proz/enzo/atividaderepeticao/tabuada.java
+++ repeticao_java/src/proz/enzo/atividaderepeticao/tabuada.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class tabuada {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int multi = 0;
-        int aux = 0;
-        while(aux <10){
-            System.out.println("digite um numero");
-            int numero = scan.nextInt();
 
-            multi = multi + numero;
-            System.out.println(multi);
-            aux++;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("digite um numero");
+        int numero = scan.nextInt();
+        int multi = 0;
+        while(multi <=10){
+            int valor = multi*numero;
+            System.out.println(valor);
+            multi++;
 
             
         }
