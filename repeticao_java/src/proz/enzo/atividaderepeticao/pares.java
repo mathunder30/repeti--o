7342980755653d17b35306d6d533d654b767b55b
Mateus Lopes <mateustlopes11@gmail.com>
2024-06-04 18:46:08 -0300

    mensagem

---
+++ repeticao_java/src/proz/enzo/atividaderepeticao/pares.java
@@ -0,0 +1,21 @@
+package proz.enzo.atividaderepeticao;
+
+import java.util.Scanner;
+
+public class pares {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o numero: ");
+        int num1 = scan.nextInt();
+        System.out.println("digite um numero: ");
+        int num2 = scan.nextInt();
+        int par = 0;
+        while (num1>= par && num2 <= par) {
+            
+            
+        }
+
+        scan.close();
+    }
+    
+}
