a756eb9eb529d620cfcfb02a8d4f784e4a90e12e
Mateus Lopes <mateustlopes11@gmail.com>
2024-06-05 19:53:53 -0300

    mensagem

--- repeticao_java/src/proz/enzo/atividaderepeticao/impares.java
+++ repeticao_java/src/proz/enzo/atividaderepeticao/impares.java
@@ -2,23 +2,20 @@ package proz.enzo.atividaderepeticao;
 
 import java.util.Scanner;
 
-public class pares {
+public class impares {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite o numero: ");
+        System.out.println("digite o primeiro numero: ");
         int num1 = scan.nextInt();
-        System.out.println("digite um numero: ");
+        System.out.println("digite o segundo numero: ");
         int num2 = scan.nextInt();
-        
         while (num1<=num2) {
-           if (num1 % 2 == 0) {
-            System.out.println(num1);
-            
-           }
-            
-           num1++; 
-        }
+            if (num1 %2 == 1){
+                System.out.println(num1);
 
+            }
+            num1 = num1 +1;
+        }
         scan.close();
     }
     
